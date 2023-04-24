7e51bdf9490d06c59df43792f5e68d8d5b542a93
PedroDiOliveira <pedro.h.soterio@gmail.com>
2023-04-23 22:01:26 -0300

    chegando no fim dos arrays e slices

--- q1/q1.go
+++ q1/q1.go
@@ -13,16 +13,26 @@ func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (floa
 	switch {
 	case currentPurchase <= float64(0):
 		return 0, fmt.Errorf("Valor da compra inválido.")
-	case somaHistorico > float64(1000):
-		disconto = somaHistorico * 0.1
-	case somaHistorico <= float64(1000) && somaHistorico >= 501:
-		disconto = somaHistorico * 0.05
-	case somaHistorico <= 500:
-		disconto = somaHistorico * 0.02
-	case somaHistorico == float64(0):
-		disconto = somaHistorico * 0.2
 	case somaHistorico/float64(len(purchaseHistory)) > float64(1000):
-		disconto = somaHistorico * 0.2
+		disconto = currentPurchase * 0.2
+	case somaHistorico > float64(1000):
+		disconto = currentPurchase * 0.1
+	case somaHistorico <= float64(1000) && somaHistorico >= 501:
+		disconto = currentPurchase * 0.05
+	case somaHistorico <= 500:
+		disconto = currentPurchase * 0.02
+	case somaHistorico == float64(0):
+		disconto = currentPurchase * 0.2
 	}
 	return disconto, nil
+}
+func main() {
+	numero := 1500.0
+	slice := []float64{800, 1200, 700, 1500}
+	resultado, err := CalculateDiscount(numero, slice)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(resultado)
+	}
 }
--- q3/q3.go
+++ q3/q3.go
@@ -1,6 +1,63 @@
 package q3
 
+import (
+	"fmt"
+	"math"
+)
+
 func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
-	// Implemente sua solução aqui
-	return 0, 0, 0, nil
+	maximo := math.Inf(-1) // Define o máximo como o menor número possível
+	minimo := math.Inf(1)  // Define o mínimo como o maior número possível
+	var soma, media, denominador float64
+	if len(numbers) <= 0 {
+		return 0, 0, 0, fmt.Errorf("Lista vazia.") // erro caso a lista seja igual a 0
+	}
+	for i := 0; i < len(numbers); i++ {
+		soma += float64(numbers[i]) //soma de todos os numeros
+		denominador++               //soma dos denominadores ate o len(numbers)
+		valorMax := numbers[i]
+		valorMin := numbers[i]
+		if float64(valorMax) > maximo {
+			maximo = float64(valorMax)
+		}
+		if float64(valorMin) < minimo {
+			minimo = float64(valorMin)
+		}
+	}
+	media = (soma - float64(minimo) - float64(maximo)/(denominador-2))
+	return int(maximo), int(minimo), media, nil
 }
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Print(FindMinMaxAverage(slice))
+
+}
+
+//ouuuu
+//package q3
+//
+//import "fmt"
+//
+//func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
+//
+//	maior := -9999999999
+//	menor := 99999999999
+//	var soma float64
+//	var contagem float64
+//	var media float64
+//	if len(numbers) == 0 {
+//		return 0, 0, 0, fmt.Errorf("Lista vazia")
+//	}
+//	for i := 0; i < len(numbers); i++ {
+//		soma += float64(numbers[i])
+//		contagem++
+//		if numbers[i] > maior {
+//			maior = numbers[i]
+//		}
+//		if numbers[i] < menor {
+//			menor = numbers[i]
+//		}
+//	}
+//	media = (soma - float64(menor) - float64(maior)) / (contagem - 2)
+//	return maior, menor, media, nil
+//}
